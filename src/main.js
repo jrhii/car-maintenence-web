83df841ba552d8637c190f540da4d7ef7dccf61b
jrhii <james.r.harter.ii@gmail.com>
2017-01-17 01:22:45 -0600

    Login Work
    Working Registration, half working login validation

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import socketio from 'socket.io';
 import http from 'http';
 import express from 'express';
 import crypto from 'crypto';
-
+import rethink from 'rethinkdb';
 import fs from 'fs';
 
 const app = new express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const DB_ADDRESS = {host: 'localhost', port: 28015};
+let connection = null;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -20,6 +23,22 @@ app.get('/test.js', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('login', (username, password) => {
+        console.log('Checking Login');
+
+        rethink.table('login').filter(rethink.row('username').eq(username)).
+            run(connection, (err, cursor) => {
+                if (err) throw err;
+                cursor.each((err, row) => {
+                    console.log(row.username);
+                    if (err) throw err;
+                    if (verifyLogin(row, password)) {
+                        console.log(`${username} login verified.`);
+                        cursor.close;
+                    } else {
+                        console.log('Bad Login');
+                    }
+                });
+            });
         //check for username, if exists, pull salt, do hash.  if no, send false
     });
 
@@ -29,18 +48,37 @@ io.on('connection', (socket) => {
         const salt = crypto.randomBytes(16);
         const hashed = crypto.pbkdf2Sync(password, salt, ITERATION, 256, 'sha256');
 
-        const store = {ITERATION : ITERATION,
+        const store = {
+            username : username,
+            ITERATION : ITERATION,
             salt : salt,
             hashed : hashed,
         };
 
-        fs.writeFileSync('testStore', JSON.stringify(store));
+        rethink.table('login').insert(store).run(connection, (err, result) => {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
     });
+});
+
+rethink.connect(DB_ADDRESS, (err, conn) => {
+    if (err) throw err;
+    connection = conn;
 });
 
 server.listen(3000, () => {
     console.log('listening on port 3000');
 });
+
+
+function verifyLogin(row, password) {
+    const freshHash = crypto.pbkdf2Sync(password, row.salt, row.ITERATION, 256, 'sha256');
+    fs.writeFileSync('testFreshHash', JSON.stringify(freshHash));
+    fs.writeFileSync('testOldHash', JSON.stringify(row.hashed));
+
+    return row.hashed.data === freshHash.data;
+}
 
 
 /******
@@ -52,13 +90,3 @@ select car
 
 display car
 ******/
-
-/*const client = new pg.Client('postgresql://postgres:no1nos@localhost/postgres');
-
-client.connect((err) => {
-    if (err) {
-        throw err;
-    } else {
-        console.log('connected');
-    }
-});*/
--- src/test.jsx
+++ src/test.jsx
@@ -5,32 +5,35 @@ class NameForm extends React.Component {
         super(props);
         this.state = {username: '', password: ''};
 
-        this.handleUsername = this.handleUsername.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
         this.socket = io();
     }
 
-    handleUsername(event) {
-        this.setState({username: event.target.value});
-    }
-
-    handlePassword(event) {
-        this.setState({password: event.target.value});
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
     }
 
     handleSubmit(event) {
-        this.socket.emit('register', this.state.username, this.state.password);
+        this.socket.emit('login', this.state.username, this.state.password);
         console.log('A name was submitted: ' + this.state.username);
+        event.preventDefault();
+    }
+
+    handleRegister(event) {
+        this.socket.emit('register', this.state.username, this.state.password);
+        console.log('A name was submitted to be registered: ' + this.state.username);
         event.preventDefault();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="username" value={this.state.username} onChange={this.handleUsername} required="required"/>
-                <input type="text" placeholder="password" value={this.state.password} onChange={this.handlePassword} required="required"/>
-                <input type="submit" value="Submit"/>
+                <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} required="required"/>
+                <input type="text" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} required="required"/>
+                <input type="submit" value="Login"/>
+                <button type="button" onClick={this.handleRegister}>Register</button>
             </form>
         );
     }
